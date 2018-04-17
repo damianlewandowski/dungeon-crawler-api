787f940adb9bb04cc7d0db3a0cd5600778440d56
damian20lewandowski@gmail.com <damian20lewandowski@gmail.com>
2018-04-17 10:35:36 +0200

    Added some middleware

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const db = require('./config/db');
 
 const app = express();
 app.set('port', process.env.PORT || 3001)
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
